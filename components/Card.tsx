a7684b783c6b8e03293033eee22b8401181e4db1
bacembenhfayehd <bacem.benhfayedh@edu.isetcom.tn>
2025-03-02 14:28:34 +0100

    add files/folders required

---
+++ components/Card.tsx
@@ -0,0 +1,19 @@
+import { View } from "react-native"
+
+export const FeaturedCard = () => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+
+export const Card = () => {
+
+    return(
+        <View>
+            
+        </View>
+    )
+}
